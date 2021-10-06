e98268c48729cb6847fd97e54e61579a35f1c752
Aissam <aissamrahmani@gmail.com>
2021-10-06 13:53:02 +0200

    modifiy variabl commad

--- XnatXport/src/main/java/org/nrg/xnat/icm/rest/XnatRunPipelineApi.java
+++ XnatXport/src/main/java/org/nrg/xnat/icm/rest/XnatRunPipelineApi.java
@@ -1005,7 +1005,7 @@ public class XnatRunPipelineApi
                 commande = "";
         }
 
-        return "";
+        return commande;
     }
 
     public String  getCommandeMacapype(String version, String inputDirBIDS){
