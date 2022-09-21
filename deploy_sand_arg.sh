1bc82d4574e7a3eb89a706ccdb821a8fdedd8e7b
David Meunier <david_meunier_79@hotmail.fr>
2022-09-21 12:01:19 +0200

    deploy -> deploy arg for xnat, and deploy_sand__arg for xnat_sand

---
+++ deploy_sand_arg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set +x
+
+echo "delete previous build"
+rm -rf build
+
+echo "Building plugin..."
+./gradlew build
+echo "Delete old jar..."
+ssh xnat@xnatsand  "rm /data/xnat/home/plugins/xnat-plugin-run-pipeline-cluster-$1.jar | exit   "
+
+cd build/libs
+echo "Copying new plugin..."
+scp xnat-plugin-run-pipeline-cluster-$1.jar xnat@xnatsand:/data/xnat/home/plugins
+
+echo "Restarting tomcat8..."
+ssh -t xnat@xnatsand "sudo systemctl restart tomcat8 | exit   "
+
